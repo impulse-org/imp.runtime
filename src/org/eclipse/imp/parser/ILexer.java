bb3b37ed6b846973869f337d812c5e7dc3a6204a
Philippe Charles <pgc1@rice.edu>
2005-10-19 21:58:59 +0000

    Deleted unused methods from interface. Renamed resetInput() to initialize() to match name on concrete lexer implementation class (and avoid necessity of wrapping with another method).

--- src/org/eclipse/imp/parser/ILexer.java
+++ src/org/eclipse/imp/parser/ILexer.java
@@ -5,14 +5,11 @@ import com.ibm.lpg.LexStream;
 import com.ibm.lpg.PrsStream;
 
 public interface ILexer {
-
-	boolean isKeyword(IToken token);
-
-	int [] getKeywordKinds();
-
-	boolean isKeywordStart(char c);
+	public int[] getKeywordKinds();
 
 	public LexStream getLexStream();
-    public void resetInput(char[] input_chars, String filename);
+
+	public void initialize(char[] contents, String filename);
+
 	public void lexer(PrsStream prsStream);
 }

681b08b47cb3757f23ffc4b9db24071a72ebe8a2
Robert M. Fuhrer <rmfuhrer@optonline.net>
2005-10-26 21:21:07 +0000

    DELAY field is now initialized at declaration site, rather than after first use :-). Commented out unused field 'editor'.

--- src/org/eclipse/imp/editor/internal/OutlineController.java
+++ src/org/eclipse/imp/editor/internal/OutlineController.java
@@ -31,85 +31,86 @@ import org.eclipse.uide.parser.IParseController;
 
 /**
  * @author Claffra
- *
- * TODO add documentation
  */
 public class OutlineController implements IContentOutlinePage, IModelListener {
     protected Tree tree;
+
     private IParseController controller;
-    private UniversalEditor editor;
+
+    //    private UniversalEditor editor;
+
     private IOutliner outliner;
+
     private UIJob job;
-//  private Language language;
-    private int DELAY;
-    
+
+    private int DELAY= 500;
+
     public OutlineController(UniversalEditor editor) {
-        this.editor = editor;
+    //	this.editor= editor;
     }
+
     public void setLanguage(Language language) {
-        outliner = (IOutliner) ExtensionPointFactory.createExtensionPoint(language, 
-                "org.eclipse.uide", "outliner");
+	outliner= (IOutliner) ExtensionPointFactory.createExtensionPoint(language, "org.eclipse.uide", "outliner");
     }
-   	public void createControl(Composite parent) {
-		tree = new Tree(parent, SWT.NONE);
-		tree.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				try {
-					TreeItem item = tree.getSelection()[0];
-					Ast node = (Ast) item.getData();
-					if (node != null) {
-//						int start = node.getStartOffset();
-//						int end = node.getEndOffset();
-					}
-				}
-				catch (Throwable ee) {
-					ErrorHandler.reportError("Universal Editor Error", ee);
-				}
-				super.widgetSelected(e);
-			}
-		});
-		if (outliner != null)
-		    outliner.setTree(tree);
-	}
-    public void dispose() {
+
+    public void createControl(Composite parent) {
+	tree= new Tree(parent, SWT.NONE);
+	tree.addSelectionListener(new SelectionAdapter() {
+	    public void widgetSelected(SelectionEvent e) {
+		try {
+		    TreeItem item= tree.getSelection()[0];
+		    Ast node= (Ast) item.getData();
+		    if (node != null) {
+			//						int start = node.getStartOffset();
+			//						int end = node.getEndOffset();
+		    }
+		} catch (Throwable ee) {
+		    ErrorHandler.reportError("Universal Editor Error", ee);
+		}
+		super.widgetSelected(e);
+	    }
+	});
+	if (outliner != null)
+	    outliner.setTree(tree);
     }
+
+    public void dispose() {}
+
     public Control getControl() {
-        return tree;
+	return tree;
     }
-    public void setActionBars(IActionBars actionBars) {
+
+    public void setActionBars(IActionBars actionBars) {}
+
+    public void setFocus() {}
+
+    public void addSelectionChangedListener(ISelectionChangedListener listener) {}
+
+    public ISelection getSelection() {
+	return null;
     }
-    public void setFocus() {
-    }
-    public void addSelectionChangedListener(ISelectionChangedListener listener) {
-    }
-    public ISelection getSelection() {	        
-        return null;
-    }
-    public void removeSelectionChangedListener(
-            ISelectionChangedListener listener) {
-    }
-    public void setSelection(ISelection selection) {
-    }
+
+    public void removeSelectionChangedListener(ISelectionChangedListener listener) {}
+
+    public void setSelection(ISelection selection) {}
+
     public void update(IParseController result, IProgressMonitor monitor) {
-        this.controller = result;
-        if (job != null)
-            job.cancel();
-        else
-            job = new UIJob("Outline View Controller") {
-	            public IStatus runInUIThread(IProgressMonitor monitor) {
-	                int offset = 0;
-                    try {
-                        if (outliner != null)
-                            outliner.createOutlinePresentation(controller, offset);
-                    }
-                    catch (Throwable e) {
-                        ErrorHandler.reportError("Outline View Controller", e);
-                    }
-	                return Status.OK_STATUS;
-	            }
-	        };
-        job.schedule(DELAY);
-        DELAY = 500;
+	this.controller= result;
+	if (job != null)
+	    job.cancel();
+	else
+	    job= new UIJob("Outline View Controller") {
+		public IStatus runInUIThread(IProgressMonitor monitor) {
+		    int offset= 0;
+		    try {
+			if (outliner != null)
+			    outliner.createOutlinePresentation(controller, offset);
+		    } catch (Throwable e) {
+			ErrorHandler.reportError("Outline View Controller", e);
+		    }
+		    return Status.OK_STATUS;
+		}
+	    };
+	job.schedule(DELAY);
     }
 }
-

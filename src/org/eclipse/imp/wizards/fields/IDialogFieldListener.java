68f116bed4e78bf3e048aa6b8b6857c24d030224
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-03-24 07:46:45 +0000

    Moved wizard dialog-related classes here from org.eclipse.uide, so they can be seen by clients.

---
+++ src/org/eclipse/imp/wizards/fields/IDialogFieldListener.java
@@ -0,0 +1,11 @@
+package org.eclipse.uide.wizards.fields;
+
+/**
+ * Change listener used by <code>DialogField</code>
+ */
+public interface IDialogFieldListener {
+    /**
+     * The dialog field has changed.
+     */
+    void dialogFieldChanged(DialogField field);
+}
---
+++ src/org/eclipse/imp/wizards/fields/IStringButtonAdapter.java
@@ -0,0 +1,13 @@
+/*
+ * Created on Feb 6, 2006
+ */
+package org.eclipse.uide.wizards.fields;
+
+/**
+ * Change listener used by <code>StringButtonDialogField</code>
+ */
+public interface IStringButtonAdapter {
+
+    void changeControlPressed(DialogField field);
+
+}

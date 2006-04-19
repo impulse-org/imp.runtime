00cad759a29cdf258b6c8734e99892eb9602e432
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-04-19 23:24:39 +0000

    New interface to represent a unary predicate taking a textual position.

---
+++ src/org/eclipse/imp/utils/IPositionPredicate.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.eclipse.uide.utils;
+
+import org.eclipse.jface.text.Position;
+
+/**
+ * Interface that represents a single-argument predicate taking a textual Position.
+ * Used by AnnotationUtils to detect annotations associated with a particular range
+ * or location in source text.
+ * @author rfuhrer
+ */
+public interface IPositionPredicate {
+    boolean matchPosition(Position p);
+}

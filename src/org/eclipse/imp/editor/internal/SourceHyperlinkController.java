1e94170112ceb5178b446747e9fa7b92c06f9bb7
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-04-20 02:08:42 +0000

    Removed unnecessary imports.

--- src/org/eclipse/imp/editor/internal/SourceHyperlinkController.java
+++ src/org/eclipse/imp/editor/internal/SourceHyperlinkController.java
@@ -6,11 +6,9 @@ package org.eclipse.uide.internal.editor;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
 import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
 import org.eclipse.uide.editor.ISourceHyperlinkDetector;
-import org.eclipse.uide.editor.UniversalEditor;
 import org.eclipse.uide.parser.IModelListener;
 import org.eclipse.uide.parser.IParseController;
 

017c910ac593c64ac19374c971ee42c7d10c087d
Stanley M. Sutton <suttons@us.ibm.com>
2006-05-30 19:54:59 +0000

    A package for builders.  The class MarkerCreator creates problem markers for errors reported from the "compiling" of a "source" file.  This function is analogous to that of AnnotationCreator, which creates annotations for errors reported from the "compiling" of a "source" file that is being viewed in an editor.  However, since AnnotationCreator is used with the editor and not builders, and since MarkerCreator is used with builders and has no involvement with editors, these are put into separate packages.

---
+++ src/org/eclipse/imp/builder/MarkerCreator.java
@@ -0,0 +1,82 @@
+package org.eclipse.uide.builder;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.uide.editor.IMessageHandler;
+import org.eclipse.uide.parser.IParseController;
+import lpg.lpgjavaruntime.IToken;
+import lpg.lpgjavaruntime.PrsStream;
+
+
+/**
+ * This class provides a message handler that creates markers in
+ * response to received messages.
+ * 
+ * MarkerCreators are instantiated with a file (IFile) and a parse
+ * controller (IParseController).  The parse controller should
+ * be parsing the file and generating the messages that are
+ * received by the MarkerCreator.  The MarkerCreator, in turn,
+ * creates a problem marker for each error message received,
+ * uses the parse controller to compute a line number for the
+ * token provided with each message, and attaches the marker to
+ * the given file at the computed line.
+ */
+
+    
+public class MarkerCreator implements IMessageHandler {
+	
+	private IParseController parseController = null;
+	private IFile file = null;
+	private String problemType = IMarker.PROBLEM;
+	
+	public MarkerCreator(
+		IFile file, IParseController parseController)
+	{
+		this.parseController = parseController;
+		this.file = file;
+	}
+
+	public MarkerCreator(
+			IFile file,
+			IParseController parseController,
+			String problemType)
+	{
+		this.file = file;
+		this.parseController = parseController;
+		this.problemType = problemType;
+	}
+	
+	
+	public void handleMessage(int offset, int length, String message) {
+		PrsStream  ps = parseController.getParser().getParseStream();
+		IToken token = ps.getTokenAtCharacter(offset);
+		int line = token.getLine();
+
+		try {
+			// Based closely on "FAQ How do I create problem markers for my compiler?"
+			IMarker m = file.createMarker(problemType);
+			m.setAttribute(IMarker.LINE_NUMBER, line);
+			// SMS 17 May 2006
+			// Note: positions of text ranges seem to be handled differently
+			// for Annotations and Markers.  Annotations are associated with a
+			// Position, which takes an offset and length.  The offset and length
+			// provided to handleMessage(..), when used in a Position, enable the
+			// annotation to be plotted in the correct place.  Markers, in contrast,
+			// get a start and end character.  When the offset and length provided
+			// to handleMessage(..) are used in these roles, the Marker is not
+			// plotted in the correct place.  In the cases I have seen, the Marker
+			// is plotted somewhat in advance of its intended location.  Adding
+			// the line number (less one) to the offset yields the correct
+			// starting character for the marker.  So that is done here.
+			int start = offset + line - 1;
+			m.setAttribute(IMarker.CHAR_START, start);
+			m.setAttribute(IMarker.CHAR_END, start + length);
+			m.setAttribute(IMarker.MESSAGE, message);
+			m.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
+			m.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
+		} catch (CoreException e) {
+			System.err.println("MarkerCreator.handleMessage:  CoreException trying to create marker");
+		}
+	}
+}

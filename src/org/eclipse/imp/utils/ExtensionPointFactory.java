12310656c0b3e1fdf5eb3527725a1999e5c14ab6
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-06-14 20:08:27 +0000

    Factored out code that creates a default service implementation. createExtensionPoint() now tries the base language, if any, if a derived language has no language-specific service impl.

--- src/org/eclipse/imp/utils/ExtensionPointFactory.java
+++ src/org/eclipse/imp/utils/ExtensionPointFactory.java
@@ -7,6 +7,7 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.uide.core.ErrorHandler;
 import org.eclipse.uide.core.ILanguageService;
 import org.eclipse.uide.core.Language;
+import org.eclipse.uide.core.LanguageRegistry;
 import org.eclipse.uide.preferences.SAFARIPreferenceCache;
 import org.eclipse.uide.runtime.RuntimePlugin;
 import org.osgi.framework.Bundle;
@@ -18,6 +19,7 @@ import org.osgi.framework.Bundle;
 
 /**
  * @author Claffra
+ * @author rfuhrer@watson.ibm.com
  */
 public class ExtensionPointFactory {
     public static ILanguageService createExtensionPoint(Language language, String pluginID, String extensionPointId) {
@@ -26,20 +28,35 @@ public class ExtensionPointFactory {
 	    return null;
 	}
 	ILanguageService service= null;
+
 	try {
 	    IExtensionPoint extensionPoint= Platform.getExtensionRegistry().getExtensionPoint(pluginID, extensionPointId);
-	    if (extensionPoint != null) {
+
+	    if (extensionPoint != null)
 		service= getLanguageContributor(extensionPoint, language.getName());
-	    }
 	} catch (Throwable e) {
 	    ErrorHandler.reportError("Error finding \"" + extensionPointId + "\" service for \"" + language + "\"", e);
 	}
+
+	// Check for a base language implementation and use that if available
+	if (service == null && language.getDerivedFrom() != null)
+	    service= createExtensionPoint(LanguageRegistry.findLanguage(language.getDerivedFrom()), pluginID, extensionPointId);
+
+	if (service == null)
+	    service= createDefaultImpl(language, pluginID, extensionPointId);
+
+	// if (service != null)
+	//   service.setLanguage(language.getName());
+	return service;
+    }
+
+    private static ILanguageService createDefaultImpl(Language language, String pluginID, String extensionPointId) {
+	ILanguageService service= null;
 	try {
-	    if (service == null) {
-		String className= "Default" + Character.toUpperCase(extensionPointId.charAt(0)) + extensionPointId.substring(1);
-		String defaultClass= pluginID + ".defaults." + className;
-		service= (ILanguageService) Class.forName(defaultClass).newInstance();
-	    }
+	    String className= "Default" + Character.toUpperCase(extensionPointId.charAt(0)) + extensionPointId.substring(1);
+	    String defaultClass= pluginID + ".defaults." + className;
+
+	    service= (ILanguageService) Class.forName(defaultClass).newInstance();
 	} catch (ClassNotFoundException e) {
 	    if (SAFARIPreferenceCache.emitMessages)
 		RuntimePlugin.getInstance().writeInfoMsg("No language-specific or default implementation found for service " + extensionPointId + " and language " + language.getName());
@@ -48,8 +65,6 @@ public class ExtensionPointFactory {
 	} catch (Throwable ee) {
 	    ErrorHandler.reportError("Universal Editor Error", ee);
 	}
-	// if (service != null)
-	// service.setLanguage(language.getName());
 	return service;
     }
 

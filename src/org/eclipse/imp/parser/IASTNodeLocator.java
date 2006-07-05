182d24037fc054817d30ed72bffbfad8533017a1
Stanley M. Sutton <suttons@us.ibm.com>
2006-07-05 19:21:07 +0000

    Added new methods to interface to make more information available about nodes that are represented generically (but otherwise unhelpfully) as Objects.

--- src/org/eclipse/imp/parser/IASTNodeLocator.java
+++ src/org/eclipse/imp/parser/IASTNodeLocator.java
@@ -3,8 +3,20 @@
  */
 package org.eclipse.uide.parser;
 
-public interface IASTNodeLocator {
+public interface IASTNodeLocator
+{
     Object findNode(Object ast, int offset);
 
     Object findNode(Object ast, int startOffset, int endOffset);
+    
+    // SMS 23 Jun 2006
+    // The following three methods are new, to avoid the need to refer
+    // to a more language-specific entity in order to get these values
+    
+    int getStartOffset(Object node);
+    
+    int getEndOffset(Object node);
+    
+    int getLength(Object  node);
+    
 }

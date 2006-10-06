28a4aed619ce57f26703f04aae9403a6c8246735
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-10-06 21:45:44 +0000

    Simplified somewhat by use of collections rather than arrays.

--- src/org/eclipse/imp/editor/ReferenceResolver.java
+++ src/org/eclipse/imp/editor/ReferenceResolver.java
@@ -1,11 +1,13 @@
 package org.eclipse.uide.editor;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import org.eclipse.uide.parser.IParseController;
 
 /**
- * Provides a default implementation for (most of) the the ILinnkMapper interface,
- * the purpose of which is to support the creation of hyperlinks between nodes
- * in an AST.
+ * Provides a default implementation for (most of) the the IReferenceResolver interface,
+ * the purpose of which is to support the creation of hyperlinks between nodes in an AST.
  * 
  * The principal methods remaining for the user to implement are
  * 		public Object getLinkTarget(Object node, IParseController parseController)
@@ -14,81 +16,42 @@ import org.eclipse.uide.parser.IParseController;
  * which will currently return not-very-useful values.
  * 
  * @author sutton
- *
  */
 public abstract class ReferenceResolver implements IReferenceResolver {
 
-	protected Class[] fSourceTypes = null;
+	protected Set<Class> fSourceTypes = new HashSet<Class>();
 	
-	public ReferenceResolver() { };
+	public ReferenceResolver() { }
 	
-	public ReferenceResolver(Class[] sourceTypes)
+	public ReferenceResolver(Collection<Class> sourceTypes)
 	{
-		fSourceTypes = sourceTypes;
+	    fSourceTypes.addAll(sourceTypes);
 	}
 	
-
-	protected boolean contains(Class[] types, Class type)
-	{
-		if (types == null || type == null) return false;
-		for (int i = 0; i < types.length; i++) {
-			if (types[i].equals(type)) return true;
-		}
-		return false;
+	public void setSourceTypes(Collection<Class> sourceTypes) {
+	    fSourceTypes.clear();
+	    fSourceTypes.addAll(sourceTypes);
 	}
-	
-	
-	public void setSourceTypes(Class[] sourceTypes) {
-		fSourceTypes = sourceTypes;
-	}
-	
 	
 	public void addSourceType (Class type)
 	{
-		if (contains(fSourceTypes, type)) return;
-		Class[] newSourceTypes = new Class[fSourceTypes.length + 1];
-		for (int i = 0; i < fSourceTypes.length; i++) {
-			newSourceTypes[i] = fSourceTypes[i];
-		}
-		newSourceTypes[newSourceTypes.length-1] = type;
-		fSourceTypes = newSourceTypes;
+	    fSourceTypes.add(type);
 	}
 
-	
 	public void removeSourceType(Class type)
 	{
-		if (!contains(fSourceTypes, type)) return;
-		Class[] newSourceTypes = new Class[fSourceTypes.length - 1];
-		int j = 0;
-		for (int i = 0; i < fSourceTypes.length; i++) {
-			if (fSourceTypes[i].equals(type)) continue;
-			newSourceTypes[j] = fSourceTypes[i];
-			j++;
-		}
-		fSourceTypes = newSourceTypes;
+	    fSourceTypes.remove(type);
 	}
-	
 
 	// If you don't wish to enumerate the source types for hyperlinks
 	// then fSourceTypes can be ignored and this method can be 
 	// overridden in the language-specific concrete subclass so as
 	// to return an appropriate value in some other way
 	public boolean hasSuitableLinkSourceType(Object node) {
-		return contains(fSourceTypes, node.getClass());
-	}
-
-	
-	public Object getLinkTarget(Object node, IParseController parseController) {
-		// TODO Auto-generated method stub
-		System.err.println("ReferenceResolver.getLinkTarget(..):  UNIMPLEMENTED; returning null");
-		return null;
-	}
-
-	
-	public String getLinkText(Object node) {
-		// TODO Auto-generated method stub
-		System.err.println("ReferenceResolver.getLinkTarget(..):  UNIMPLEMENTED; returning 'Text unspecified'");
-		return "Text unspecified";
+		return fSourceTypes.contains(node.getClass());
 	}
 	
+	public abstract Object getLinkTarget(Object node, IParseController parseController);
+	
+	public abstract String getLinkText(Object node);
 }

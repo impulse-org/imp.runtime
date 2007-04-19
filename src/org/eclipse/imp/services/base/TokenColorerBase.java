8514692d30f33ad7d78d0703a56630348af6ce8b
Stanley M. Sutton <suttons@us.ibm.com>
2007-04-19 20:24:36 +0000

    New base type for token colorers, intended to serve as a replacement for the older DefaultTokenColorer.  The simple token colorer template has also been revised to refer to this new base class.

---
+++ src/org/eclipse/imp/services/base/TokenColorerBase.java
@@ -0,0 +1,32 @@
+package org.eclipse.uide.defaults;
+
+import lpg.runtime.IToken;
+
+import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.uide.editor.ITokenColorer;
+import org.eclipse.uide.parser.IParseController;
+
+public class TokenColorerBase implements ITokenColorer {
+
+    TextAttribute keywordAttribute;
+
+    public TextAttribute getColoring(IParseController controller, IToken token) {
+        switch (token.getKind()) {
+            default:
+                if (controller.isKeyword(token.getKind()))
+                     return keywordAttribute;
+               else return null;
+        }
+    }
+
+    public TokenColorerBase() {
+    	this(new TextAttribute(Display.getDefault().getSystemColor(SWT.COLOR_DARK_MAGENTA), null, SWT.BOLD));
+    }
+
+    public TokenColorerBase(TextAttribute keywordAttribute) {
+        this.keywordAttribute = keywordAttribute;
+    }
+
+}

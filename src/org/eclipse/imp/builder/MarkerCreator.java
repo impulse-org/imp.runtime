9c693c5950544db191809250d758a4cfa12e49f4
Stanley M. Sutton <suttons@us.ibm.com>
2007-04-23 19:39:04 +0000

    Removed previously added adjustment to position of marker annotation that seems to have been rendered unnecessary by fixes elsewhere.

--- src/org/eclipse/imp/builder/MarkerCreator.java
+++ src/org/eclipse/imp/builder/MarkerCreator.java
@@ -50,32 +50,23 @@ public class MarkerCreator implements IMessageHandler {
 	
 	
 	public void handleMessage(int errorCode, int [] msgLocation, int[] errorLocation, String filename, String [] errorInfo) {
-            int offset = msgLocation[IMessageHandler.OFFSET_INDEX],
-                length = msgLocation[IMessageHandler.LENGTH_INDEX];
-            String message = "";
-            for (int i = 0; i < errorInfo.length; i++)
-                message += (errorInfo[i] + (i < errorInfo.length - 1 ? " " : ""));
-            IPrsStream  ps = parseController.getParser().getParseStream();
-            IToken token = ps.getTokenAtCharacter(offset);
-            int line = token.getLine();
+        int offset = msgLocation[IMessageHandler.OFFSET_INDEX],
+            length = msgLocation[IMessageHandler.LENGTH_INDEX];
+        String message = "";
+        for (int i = 0; i < errorInfo.length; i++)
+            message += (errorInfo[i] + (i < errorInfo.length - 1 ? " " : ""));
+        IPrsStream  ps = parseController.getParser().getParseStream();
+        IToken token = ps.getTokenAtCharacter(offset);
+        int line = token.getLine();
 
 		try {
 			// Based closely on "FAQ How do I create problem markers for my compiler?"
 			IMarker m = file.createMarker(problemType);
 			m.setAttribute(IMarker.LINE_NUMBER, line);
-			// SMS 17 May 2006
-			// Note: positions of text ranges seem to be handled differently
-			// for Annotations and Markers.  Annotations are associated with a
-			// Position, which takes an offset and length.  The offset and length
-			// provided to handleMessage(..), when used in a Position, enable the
-			// annotation to be plotted in the correct place.  Markers, in contrast,
-			// get a start and end character.  When the offset and length provided
-			// to handleMessage(..) are used in these roles, the Marker is not
-			// plotted in the correct place.  In the cases I have seen, the Marker
-			// is plotted somewhat in advance of its intended location.  Adding
-			// the line number (less one) to the offset yields the correct
-			// starting character for the marker.  So that is done here.
-			int start = offset + line - 1;
+			// SMS 23 Apr 2007
+			// Removed previously added adjustment to start offset that seems
+			// to have been rendered unnecessary by a fix somewhere else
+			int start = offset;
 			m.setAttribute(IMarker.CHAR_START, start);
 			m.setAttribute(IMarker.CHAR_END, start + length);
 			m.setAttribute(IMarker.MESSAGE, message);

e57c51ea02e87b158d58d3a2091f65e1aa58d69b
Robert M. Fuhrer <rmfuhrer@optonline.net>
2007-05-03 19:33:03 +0000

    Added PathEntryTypes PROJECT and CONTAINER, and added JavaDoc to each type. Renamed getPath() to getProjectRelativePath() for clarity.

--- src/org/eclipse/imp/model/IPathEntry.java
+++ src/org/eclipse/imp/model/IPathEntry.java
@@ -7,8 +7,37 @@ import org.eclipse.core.runtime.IPath;
 
 public interface IPathEntry {
     public enum PathEntryType {
-        SOURCE_FOLDER, ARCHIVE
+        /**
+         * Path entry corresponding to a source folder in the referencing project. The
+         * associated path must be absolute (not project-relative).
+         **/
+	SOURCE_FOLDER,
+        /**
+         * Path entry corresponding to an archive (jar, zip) somewhere in the workspace.
+         * The associated path must be absolute (not project-relative).
+         **/
+	ARCHIVE,
+        /**
+         * Path entry corresponding to a project dependency. The
+         * associated path must be absolute (not project-relative).
+         **/
+	PROJECT,
+        /**
+         * Path entry corresponding to some form of pseudo-container (not an archive),
+         * e.g. the set of plugin dependencies. Not sure what the path means in this
+         * case.
+         **/
+	CONTAINER
     }
-    IPathEntry.PathEntryType getEntryType();
-    IPath getProjectRelativePath();
+
+    /**
+     * @return the type of this path entry
+     */
+    PathEntryType getEntryType();
+
+    /**
+     * @return the path associated with this path entry. Different entry types may have
+     * different interpretations for the associated path. @see PathEntryType
+     */
+    IPath getPath();
 }

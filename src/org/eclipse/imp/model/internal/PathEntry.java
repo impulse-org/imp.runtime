a2b805741a25ada1cc479a3a268034a74e32dbec
Robert M. Fuhrer <rmfuhrer@optonline.net>
2007-07-05 20:56:49 +0000

    Cosmetic changes.

--- src/org/eclipse/imp/model/internal/PathEntry.java
+++ src/org/eclipse/imp/model/internal/PathEntry.java
@@ -10,16 +10,20 @@ import org.eclipse.uide.model.IPathEntry.PathEntryType;
 public class PathEntry implements IPathEntry {
     private final IPath fPath;
     private final PathEntryType fType;
+
     public PathEntry(PathEntryType type, IPath path) {
         fType= type;
         fPath= path;
     }
+
     public PathEntryType getEntryType() {
         return fType;
     }
+
     public IPath getPath() {
         return fPath;
     }
+
     public String toString() {
 	return "<" + fType + ": " + fPath.toPortableString() + ">";
     }

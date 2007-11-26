f6749adac1c017a2e62bda8409571dd1013262f7
Stanley M. Sutton <suttons@us.ibm.com>
2007-11-26 21:05:33 +0000

    Inteneded to be of general use for validators and filters used in IMP UI components.  Initially contains methods to test the kind of a given project.

---
+++ src/org/eclipse/imp/utils/ValidationUtils.java
@@ -0,0 +1,49 @@
+package org.eclipse.imp.utils;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.imp.core.ErrorHandler;
+import org.eclipse.imp.ui.dialogs.providers.ContentProviderForIDEProjects;
+import org.eclipse.ui.dialogs.ISelectionValidator;
+
+public class ValidationUtils {
+
+	public static boolean isJavaProject(IProject project) {
+	    if (project == null) return false;
+	    try {
+	    	return (project.exists() && project.hasNature("org.eclipse.jdt.core.javanature"));	
+	    } catch (CoreException e) {
+	    	ErrorHandler.reportError(
+	    		"ValidatorUtils.isPluginProject:  Core exception validating project = " + project.getName()
+	    		+ "; returning false", e);
+	    	return false;
+	    }
+	}
+	
+	
+	public static boolean isPluginProject(IProject project) {
+	    if (project == null) return false;
+	    try {
+	    	return (project.exists() && project.hasNature("org.eclipse.pde.PluginNature"));	
+	    } catch (CoreException e) {
+	    	ErrorHandler.reportError(
+	    		"ValidatorUtils.isPluginProject:  Core exception validating project = " + project.getName()
+	    		+ "; returning false", e);
+	    	return false;
+	    }
+	}
+	
+
+	public static boolean isIDEProject(IProject project) {
+		ContentProviderForIDEProjects provider = new ContentProviderForIDEProjects();
+		IProject[] ideProjects = provider.getProjects();
+		for (int i = 0; i < ideProjects.length; i++) {
+			if (ideProjects[i].equals(project))
+				return true;
+		}
+		return false;
+	}
+	
+}

a2000fc48e43962f4e41c2071e31c7fb09c7deee
Stanley M. Sutton <suttons@us.ibm.com>
2007-11-28 20:42:23 +0000

    New viewer filter for Java projects.

---
+++ src/org/eclipse/imp/ui/dialogs/filters/ViewerFilterForJavaProjects.java
@@ -0,0 +1,34 @@
+package org.eclipse.imp.ui.dialogs.filters;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.imp.utils.ExtensionUtils;
+import org.eclipse.imp.utils.ValidationUtils;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.pde.core.plugin.IExtensions;
+import org.eclipse.pde.core.plugin.IPluginExtension;
+import org.eclipse.pde.core.plugin.IPluginModel;
+
+public class ViewerFilterForJavaProjects extends ViewerFilter {
+	
+    /**
+     * Returns whether the given element makes it through this filter.
+     *
+     * @param viewer the viewer
+     * @param parentElement the parent element
+     * @param element the element
+     * @return <code>true</code> if element is included in the
+     *   filtered set, and <code>false</code> if excluded
+     */
+    public boolean select(Viewer viewer, Object parentElement,
+            Object element)
+    {
+    	if (element instanceof IProject) {
+    		if (ValidationUtils.isJavaProject((IProject) element)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+
+}

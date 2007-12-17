e80dc3d97d71d006bbe197a328395afae460180e
Robert M. Fuhrer <rmfuhrer@optonline.net>
2007-12-17 18:22:10 +0000

    New class to represent invocation of unimplemented operations.

---
+++ src/org/eclipse/imp/utils/UnimplementedError.java
@@ -0,0 +1,13 @@
+package org.eclipse.imp.utils;
+
+public class UnimplementedError extends Error {
+    private static final long serialVersionUID= 5077799745785575339L;
+
+    public UnimplementedError() {
+        super();
+    }
+
+    public UnimplementedError(String s) {
+        super(s);
+    }
+}

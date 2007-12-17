e8cd793992299a75bdb0ddde9d6df41937b93d5e
Robert M. Fuhrer <rmfuhrer@optonline.net>
2007-12-17 18:18:16 +0000

    Added new method calculateDamageExtent() to permit languages to determine the full extent of the damage region. getColoring() now takes an Object as the entity to color, rather than an LPG-specific IToken.

--- src/org/eclipse/imp/services/ITokenColorer.java
+++ src/org/eclipse/imp/services/ITokenColorer.java
@@ -5,10 +5,9 @@
  */
 package org.eclipse.imp.services;
 
-import lpg.runtime.IToken;
-
 import org.eclipse.imp.language.ILanguageService;
 import org.eclipse.imp.parser.IParseController;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.TextAttribute;
 
 /*
@@ -17,15 +16,19 @@ import org.eclipse.jface.text.TextAttribute;
  */
 
 /**
- * @author Claffra
- *
  */
 public interface ITokenColorer extends ILanguageService {
+    /**
+     * @param seed the nominal damage area
+     * @return a possibly expanded region of damage
+     */
+    IRegion calculateDamageExtent(IRegion seed);
+
     /**
      * Provide coloring and font to use for a given token in the model.
      * @param model The model that contains the token
      * @param token The token to be colored
      * @return a TextAttribute
      */
-    public TextAttribute getColoring(IParseController controller, IToken token);
+    public TextAttribute getColoring(IParseController controller, Object token);
 }

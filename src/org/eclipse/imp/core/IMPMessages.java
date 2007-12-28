053c9db5541faa52244126c2d609901050001188
Robert M. Fuhrer <rmfuhrer@optonline.net>
2007-12-28 21:48:45 +0000

    Added some message definitions for the Project Explorer and for searching (not yet used).

--- src/org/eclipse/imp/core/IMPMessages.java
+++ src/org/eclipse/imp/core/IMPMessages.java
@@ -5,6 +5,7 @@
  */
 package org.eclipse.imp.core;
 
+import java.text.MessageFormat;
 import org.eclipse.osgi.util.NLS;
 
 public class IMPMessages extends NLS {
@@ -15,7 +16,33 @@ public class IMPMessages extends NLS {
 
     public static String Assert_null_argument;
 
+    public static String manager_filesToIndex;
+
+    public static String manager_indexingInProgress;
+
+    public static String process_name;
+
+    public static String engine_searching;
+
+    public static String engine_searching_indexing;
+
+    public static String engine_searching_matching;
+
+    public static String StatusBarUpdater_num_elements_selected;
+
+    public static String OpenWithMenu_label;
+
+    public static String EditorUtility_concatModifierStrings;
+
     private IMPMessages() {
+    }
+
+    public static String format(String message, Object object) {
+        return MessageFormat.format(message, new Object[] { object});
+    }
+
+    public static String format(String message, Object[] objects) {
+        return MessageFormat.format(message, objects);
     }
 
     static {

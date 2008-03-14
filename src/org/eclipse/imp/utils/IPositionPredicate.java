01c5c3666f151ea8f1088479d3f55e9dd681a67a
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-03-14 23:53:30 +0000

    Added EPL copyright header.

--- src/org/eclipse/imp/utils/IPositionPredicate.java
+++ src/org/eclipse/imp/utils/IPositionPredicate.java
@@ -1,8 +1,15 @@
-/*
- * (C) Copyright IBM Corporation 2007
- * 
- * This file is part of the Eclipse IMP.
- */
+/*******************************************************************************
+* Copyright (c) 2007 IBM Corporation.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+
+*******************************************************************************/
+
 /**
  * 
  */

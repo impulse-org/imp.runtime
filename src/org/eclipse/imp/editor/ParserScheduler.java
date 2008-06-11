9784b93ee9f780e7b05c68cc263dda849695f0b1
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-06-11 20:01:48 +0000

    Extracted ParserScheduler to its own non-nested class, for use with other editors.

---
+++ src/org/eclipse/imp/editor/ParserScheduler.java
@@ -0,0 +1,119 @@
+package org.eclipse.imp.editor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.imp.core.ErrorHandler;
+import org.eclipse.imp.language.Language;
+import org.eclipse.imp.model.ISourceProject;
+import org.eclipse.imp.model.ModelFactory;
+import org.eclipse.imp.parser.IMessageHandler;
+import org.eclipse.imp.parser.IModelListener;
+import org.eclipse.imp.parser.IParseController;
+import org.eclipse.imp.preferences.PreferenceCache;
+import org.eclipse.imp.runtime.RuntimePlugin;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+
+/**
+ * Parsing may take a long time, and is not done inside the UI thread. Therefore, we create a job that is executed in a
+ * background thread by the platform's job service.
+ */
+// TODO Perhaps this should be driven off of the "IReconcilingStrategy" mechanism?
+public class ParserScheduler extends Job {
+    private final IParseController fParseController;
+
+    private final IEditorPart fEditorPart;
+
+    private final Language fLanguage;
+
+    private final IDocumentProvider fDocumentProvider;
+
+    private final IMessageHandler fMsgHandler;
+
+    private final List<IModelListener> fAstListeners= new ArrayList<IModelListener>();
+
+    public ParserScheduler(IParseController parseController, Language lang, IEditorPart editorPart,
+            IDocumentProvider docProvider, IMessageHandler msgHandler) {
+        super(lang.getName() + " Parser");
+        setSystem(true); // do not show this job in the Progress view
+        fParseController= parseController;
+        fLanguage= lang;
+        fEditorPart= editorPart;
+        fDocumentProvider= docProvider;
+        fMsgHandler= msgHandler;
+    }
+
+    public IStatus run(IProgressMonitor monitor) {
+        if (fParseController == null || fDocumentProvider == null) {
+            /* Editor was closed, or no parse controller */
+            return Status.OK_STATUS;
+        }
+
+        IEditorInput editorInput= fEditorPart.getEditorInput();
+        try {
+            IDocument document= fDocumentProvider.getDocument(editorInput);
+            IFile file= EditorInputUtils.getFile(editorInput);
+            IPath filePath= EditorInputUtils.getPath(editorInput);
+
+            if (PreferenceCache.emitMessages)
+                RuntimePlugin.getInstance().writeInfoMsg(
+                        "Parsing language " + fLanguage.getName() + " for input " + editorInput.getName());
+
+            // Don't need to retrieve the AST; we don't need it.
+            // Just make sure the document contents gets parsed once (and only once).
+            fMsgHandler.clearMessages();
+            ISourceProject srcProject= (file != null) ? ModelFactory.open(file.getProject()) : null;
+            // TODO Do we really need to re-initialize the IParseController every time?
+            fParseController.initialize(filePath, srcProject, fMsgHandler);
+            fParseController.parse(document.get(), false, monitor);
+            if (!monitor.isCanceled())
+                notifyAstListeners(fParseController, monitor);
+        } catch (Exception e) {
+            ErrorHandler.reportError("Error running parser for " + fLanguage.getName() + ":", e);
+            if (PreferenceCache.emitMessages)
+                RuntimePlugin.getInstance().writeInfoMsg(
+                        "Parsing failed for language " + fLanguage.getName() + " and input " + editorInput.getName());
+            // RMF 8/2/2006 - Notify the AST listeners even on an exception - the compiler front end
+            // may have failed at some phase, but there may be enough info to drive IDE services.
+            notifyAstListeners(fParseController, monitor);
+        }
+        return Status.OK_STATUS;
+    }
+
+    public void addModelListener(IModelListener listener) {
+        fAstListeners.add(listener);
+    }
+
+    public void notifyAstListeners(IParseController parseController, IProgressMonitor monitor) {
+        // Suppress the notification if there's no AST (e.g. due to a parse error)
+        if (parseController != null) {
+            if (PreferenceCache.emitMessages)
+                RuntimePlugin.getInstance().writeInfoMsg(
+                        "Notifying AST listeners of change in " + parseController.getPath().toPortableString());
+            for(int n= fAstListeners.size() - 1; n >= 0 && !monitor.isCanceled(); n--) {
+                IModelListener listener= fAstListeners.get(n);
+                // Pretend to get through the highest level of analysis so all services execute (for now)
+                int analysisLevel= IModelListener.AnalysisRequired.POINTER_ANALYSIS.level();
+
+                if (parseController.getCurrentAst() == null)
+                    analysisLevel= IModelListener.AnalysisRequired.LEXICAL_ANALYSIS.level();
+                // TODO How to tell how far we got with the source analysis? The IAnalysisController should tell us!
+                // TODO Rename IParseController to IAnalysisController
+                // TODO Compute the minimum amount of analysis sufficient for all current listeners, and pass that to
+                // the IAnalysisController.
+                if (listener.getAnalysisRequired().level() <= analysisLevel)
+                    listener.update(parseController, monitor);
+            }
+        } else if (PreferenceCache.emitMessages)
+            RuntimePlugin.getInstance().writeInfoMsg("No AST; bypassing listener notification.");
+    }
+}

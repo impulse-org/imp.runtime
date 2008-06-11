cdebf9894f1428ecbf0c9e6a5b9d679753d328aa
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-06-11 19:48:04 +0000

    Added method clearMessages() to give implementations notification before issuing any messages.

--- src/org/eclipse/imp/parser/IMessageHandler.java
+++ src/org/eclipse/imp/parser/IMessageHandler.java
@@ -12,20 +12,41 @@
 
 package org.eclipse.imp.parser;
 
+/**
+ * This interface describes something that can process messages emitted
+ * by a parser or other language processing front-end.
+ */
 public interface IMessageHandler {
     /**
-     * @param msg
+     * Clear all previously-issued messages. Typically called at the
+     * beginning of a parsing "session".
+     */
+    void clearMessages();
+
+    /**
+     * Issue a single message with the given text and source position.
+     * @param msg the message text
      * @param startOffset 0-based, inclusive
      * @param endOffset 0-based, inclusive
-     * @param startCol ?-based, inclusive
-     * @param endCol ?-based, inclusive
-     * @param startLine ?-based, inclusive
-     * @param endLine ?-based, inclusive
+     * @param startCol 1-based, inclusive
+     * @param endCol 1-based, inclusive
+     * @param startLine 1-based, inclusive
+     * @param endLine 1-based, inclusive
      */
     void handleSimpleMessage(String msg, int startOffset, int endOffset,
             int startCol, int endCol,
             int startLine, int endLine);
 
+    /**
+     * Begins a group of related messages (e.g. the first describing an error
+     * relating multiple entities, identifying the position of the first entity,
+     * followed by individual messages, one per additional entity).
+     * @param groupName
+     */
     void startMessageGroup(String groupName);
+
+    /**
+     * Ends a group of related messages
+     */
     void endMessageGroup();
 }

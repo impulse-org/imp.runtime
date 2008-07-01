6c7826f445adb43a160a481b636d0e4796b00c90
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-07-01 17:52:25 +0000

    Added some provisional support for a new dynamic context help service.

--- src/org/eclipse/imp/editor/LanguageServiceManager.java
+++ src/org/eclipse/imp/editor/LanguageServiceManager.java
@@ -16,6 +16,7 @@ import org.eclipse.imp.runtime.RuntimePlugin;
 import org.eclipse.imp.services.IContentProposer;
 import org.eclipse.imp.services.IDocumentationProvider;
 import org.eclipse.imp.services.IFoldingUpdater;
+import org.eclipse.imp.services.IHelpService;
 import org.eclipse.imp.services.IHoverHelper;
 import org.eclipse.imp.services.ILanguageActionsContributor;
 import org.eclipse.imp.services.IOccurrenceMarker;
@@ -70,6 +71,8 @@ public class LanguageServiceManager {
 
     private Set<IModelListener> fEditorServices;
 
+    private IHelpService fContextHelper;
+
     public Language getLanguage() {
         return fLanguage;
     }
@@ -83,27 +86,26 @@ public class LanguageServiceManager {
         if (PreferenceCache.emitMessages)
             RuntimePlugin.getInstance().writeInfoMsg("Instantiating language service extensions for " + fLanguage.getName());
 
-        fHyperLinkDetector= fServiceFactory.getSourceHyperlinkDetector(fLanguage);
-
-        if (fHyperLinkDetector == null)
-            fHyperLinkDetector= new HyperlinkDetector(fLanguage);
-
         fActionContributors= fServiceFactory.getLanguageActionsContributors(fLanguage);
         fAnnotationHover= fServiceFactory.getAnnotationHover(fLanguage);
         fAutoEditStrategy= fServiceFactory.getAutoEditStrategy(fLanguage);
         fContentProposer= fServiceFactory.getContentProposer(fLanguage);
+        fContextHelper= fServiceFactory.getContextHelper(fLanguage);
         fDocProvider= fServiceFactory.getDocumentationProvider(fLanguage);
         fEditorServices= fServiceFactory.getEditorServices(fLanguage);
         fFoldingUpdater= fServiceFactory.getFoldingUpdater(fLanguage);
         fFormattingStrategy= fServiceFactory.getSourceFormatter(fLanguage);
+        fHyperLinkDetector= fServiceFactory.getSourceHyperlinkDetector(fLanguage);
         fImageProvider= fServiceFactory.getElementImageProvider(fLanguage);
         fModelBuilder= fServiceFactory.getTreeModelBuilder(fLanguage);
         fLabelProvider= fServiceFactory.getLabelProvider(fLanguage);
+        fOccurrenceMarker = fServiceFactory.getOccurrenceMarker(fLanguage);
         fParseController= fServiceFactory.getParseController(fLanguage);
         fRefactoringContributors= fServiceFactory.getRefactoringContributors(fLanguage);
         fResolver= fServiceFactory.getReferenceResolver(fLanguage);
-        
-        fOccurrenceMarker = fServiceFactory.getOccurrenceMarker(fLanguage);
+
+        if (fHyperLinkDetector == null)
+            fHyperLinkDetector= new HyperlinkDetector(fLanguage);
 
         if (fParseController == null) {
             ErrorHandler.reportError("Unable to instantiate parser for " + fLanguage.getName()
@@ -179,8 +181,10 @@ public class LanguageServiceManager {
     public IContentProposer getContentProposer() {
         return fContentProposer;
     }
-    
-    
+
+    public IHelpService getContextHelp() {
+        return fContextHelper;
+    }
     
     private static HashMap<IEditorPart, LanguageServiceManager> editorServiceMap = new HashMap<IEditorPart, LanguageServiceManager>();
     

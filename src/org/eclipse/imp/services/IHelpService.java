b230281ade26ca55d1708f830287a9514340ba3c
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-07-02 12:48:37 +0000

    Added another flavor of getHelp() that takes an IRegion of text rather than a model object, so that clients can examine the selected text, rather than the innermost construct that contains it (as, for example, provided by the node locator).

--- src/org/eclipse/imp/services/IHelpService.java
+++ src/org/eclipse/imp/services/IHelpService.java
@@ -2,6 +2,7 @@ package org.eclipse.imp.services;
 
 import org.eclipse.imp.language.ILanguageService;
 import org.eclipse.imp.parser.IParseController;
+import org.eclipse.jface.text.IRegion;
 
 public interface IHelpService extends ILanguageService {
     /**
@@ -9,4 +10,10 @@ public interface IHelpService extends ILanguageService {
      * @return the help text for the given entity, possibly HTML formatted
      */
     String getHelp(Object target, IParseController parseController);
+
+    /**
+     * @param target a selected text region for which help was requested
+     * @return the help text for the given entity, possibly HTML formatted
+     */
+    String getHelp(IRegion target, IParseController parseController);
 }

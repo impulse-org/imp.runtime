acd099c3227e7f3f0eb2a56fdd22f5e1cbbc464a
Stanley M. Sutton <suttons@us.ibm.com>
2008-09-12 03:10:25 +0000

    Added implements of IHoverHelper.

--- src/org/eclipse/imp/services/base/HoverHelperBase.java
+++ src/org/eclipse/imp/services/base/HoverHelperBase.java
@@ -14,6 +14,7 @@ package org.eclipse.imp.services.base;
 
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.imp.language.Language;
+import org.eclipse.imp.services.IHoverHelper;
 
 /**
 
@@ -22,7 +23,7 @@ import org.eclipse.imp.language.Language;
  * Updates:
  * SMS 12 Aug 2007:  
  */
-public abstract class HoverHelperBase
+public abstract class HoverHelperBase implements IHoverHelper
 {
 	
 	protected UniversalEditor fEditor = null;
@@ -36,5 +37,4 @@ public abstract class HoverHelperBase
 	public void setLanguage(Language language) {
 		fLanguage = language;
 	}
-	
 }

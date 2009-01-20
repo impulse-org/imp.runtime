5d60b8bbf44f92a9cd2fbdff9737de502c13051b
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-01-20 22:01:50 +0000

    Removed the field MODIFIED_MARK, which was used to specify a prefix added to a field's label when the field was modified; now, the color of the field label is changed instead, in order to avoid layout problems.

--- src/org/eclipse/imp/preferences/Markings.java
+++ src/org/eclipse/imp/preferences/Markings.java
@@ -16,9 +16,7 @@ public class Markings {
 
 	private Markings() {};	
 	
-	static public final String MODIFIED_MARK = ">";
-	static public final String MODIFIED_NOTE = "Modified fields are marked by an initial \"" +
-		MODIFIED_MARK + "\"";
+	static public final String MODIFIED_NOTE = "Modified fields are marked in red.";
 	
 	static public final String TAB_ERROR_MARK = "**";
 	static public final String TAB_ERROR_NOTE = "Tabs with erroneous fields are marked with \"" +

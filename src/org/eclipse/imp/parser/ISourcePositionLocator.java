57b30a7e14a7fc8f8eb414a5d60e87ed35abf0be
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-12 13:44:53 +0000

    Renamed a few formal parameters.

--- src/org/eclipse/imp/parser/ISourcePositionLocator.java
+++ src/org/eclipse/imp/parser/ISourcePositionLocator.java
@@ -24,7 +24,7 @@ public interface ISourcePositionLocator
      * @param offset the textual offset, in characters
      * @return the innermost AST node whose textual extent contains the given text offset
      */
-    Object findNode(Object ast, int offset);
+    Object findNode(Object astRoot, int offset);
 
     /**
      * @param ast the root of the AST
@@ -32,7 +32,7 @@ public interface ISourcePositionLocator
      * @param endOffset the end of the textual extent, in characters
      * @return the innermost AST node whose textual extent completely contains the given text extent
      */
-    Object findNode(Object ast, int startOffset, int endOffset);
+    Object findNode(Object astRoot, int startOffset, int endOffset);
 
     /**
      * @param entity the program entity, e.g. AST node, token, or some
@@ -51,18 +51,18 @@ public interface ISourcePositionLocator
      * 
      * 
      */
-    int getEndOffset(Object node);
+    int getEndOffset(Object entity);
 
     /**
      * @param entity the program entity, e.g. AST node, token, or some
      * kind of type system object
      * @return the length, in characters, of the textual extent spanned by the given AST node
      */
-    int getLength(Object node);
+    int getLength(Object entity);
 
     /**
      * @return the workspace-relative or file-system absolute path to the compilation unit
-     * (source or compiled, if no source) that contains the given AST node.
+     * (source or compiled, if no source) that contains the given AST node or ISourceEntity.
      * The path is in "portable" format, using the Eclipse convention '/' for the path
      * component separator.
      * @see org.eclipse.core.runtime.IPath#toPortableString()

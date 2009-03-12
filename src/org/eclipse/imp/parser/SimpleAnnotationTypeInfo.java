99e78a4897547174b2e33de9a3d23c7d66689934
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-12 19:09:02 +0000

    Added type argument to return type of getProblemMarkerTypes().

--- src/org/eclipse/imp/parser/SimpleAnnotationTypeInfo.java
+++ src/org/eclipse/imp/parser/SimpleAnnotationTypeInfo.java
@@ -25,7 +25,7 @@ public class SimpleAnnotationTypeInfo implements IAnnotationTypeInfo {
 
     private static List<String> problemMarkerTypes= new ArrayList<String>();
 
-    public List getProblemMarkerTypes() {
+    public List<String> getProblemMarkerTypes() {
         return problemMarkerTypes;
     }
 

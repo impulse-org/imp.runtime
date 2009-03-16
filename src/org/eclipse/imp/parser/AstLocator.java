536fc6585fcef62c0b198a767d7050015f3cbfd5
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-16 22:09:47 +0000

    Reindented.

--- src/org/eclipse/imp/parser/AstLocator.java
+++ src/org/eclipse/imp/parser/AstLocator.java
@@ -1,14 +1,13 @@
 /*******************************************************************************
-* Copyright (c) 2007 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
-*******************************************************************************/
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.imp.parser;
 
@@ -17,16 +16,19 @@ import lpg.runtime.IAst;
 import org.eclipse.core.runtime.IPath;
 
 /**
- * Locator implementation that works for LPG-generated AST's using the base IAst interface.
+ * Locator implementation that works for LPG-generated AST's using the base IAst
+ * interface.
+ * 
  * @author rfuhrer
  */
 public class AstLocator implements ISourcePositionLocator {
     public Object findNode(Object node, int offset) {
-	if (!(node instanceof IAst))
-	    return node;
+        if (!(node instanceof IAst))
+            return node;
         IAst astNode= (IAst) node;
 
-        if (offset >= astNode.getLeftIToken().getStartOffset() && offset <= astNode.getRightIToken().getEndOffset())
+        if (offset >= astNode.getLeftIToken().getStartOffset()
+                && offset <= astNode.getRightIToken().getEndOffset())
             return astNode;
 
         if (astNode.getAllChildren() == null)
@@ -43,22 +45,22 @@ public class AstLocator implements ISourcePositionLocator {
     public Object findNode(Object ast, int startOffset, int endOffset) {
         throw new UnsupportedOperationException();
     }
-    
+
     public int getStartOffset(Object node) {
-        IAst n = (IAst) node;
+        IAst n= (IAst) node;
         return n.getLeftIToken().getStartOffset();
     }
-    
+
     public int getEndOffset(Object node) {
-        IAst n = (IAst) node;
+        IAst n= (IAst) node;
         return n.getRightIToken().getEndOffset();
     }
-    
-    public int getLength(Object  node) {
-    	return getEndOffset(node) - getStartOffset(node);
+
+    public int getLength(Object node) {
+        return getEndOffset(node) - getStartOffset(node);
     }
 
     public IPath getPath(Object node) {
-	return null;
+        return null;
     }
 }

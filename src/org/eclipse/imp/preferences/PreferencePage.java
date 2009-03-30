f0307856f6639518679e973634f66b05c4d24e5e
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-30 22:02:39 +0000

    Added an "emit builder diagnostics" field.

--- src/org/eclipse/imp/preferences/PreferencePage.java
+++ src/org/eclipse/imp/preferences/PreferencePage.java
@@ -22,36 +22,39 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 
 public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
     public PreferencePage() {
-	super(GRID);
-	setPreferenceStore(RuntimePlugin.getInstance().getPreferenceStore());
-	setDescription("Preferences for the IMP framework");
+        super(GRID);
+        setPreferenceStore(RuntimePlugin.getInstance().getPreferenceStore());
+        setDescription("Preferences for the IMP framework");
     }
 
     public void createFieldEditors() {
-	final BooleanFieldEditor emitMessagesField= new BooleanFieldEditor(PreferenceConstants.P_EMIT_MESSAGES,
-		"E&mit diagnostic messages from IMP UI", getFieldEditorParent());
-	addField(emitMessagesField);
+        final BooleanFieldEditor emitMessagesField= new BooleanFieldEditor(PreferenceConstants.P_EMIT_MESSAGES, "E&mit diagnostic messages from IMP UI",
+                getFieldEditorParent());
+        addField(emitMessagesField);
 
-        final BooleanFieldEditor dumpTokensField= new BooleanFieldEditor(PreferenceConstants.P_DUMP_TOKENS,
-                "&Dump tokens after scanning", getFieldEditorParent());
+        final BooleanFieldEditor dumpTokensField= new BooleanFieldEditor(PreferenceConstants.P_DUMP_TOKENS, "&Dump tokens after scanning",
+                getFieldEditorParent());
         addField(dumpTokensField);
 
-	final IntegerFieldEditor tabWidthField= new IntegerFieldEditor(PreferenceConstants.P_TAB_WIDTH,
-		"&Tab width:", getFieldEditorParent());
-	tabWidthField.setValidRange(1, 16);
-	tabWidthField.setTextLimit(2);
-	tabWidthField.setEmptyStringAllowed(false);
+        final BooleanFieldEditor emitBuilderDiagnosticsField= new BooleanFieldEditor(PreferenceConstants.P_EMIT_BUILDER_DIAGNOSTICS, "Emit diagnostic messages while building",
+                getFieldEditorParent());
+        addField(emitBuilderDiagnosticsField);
+
+        final IntegerFieldEditor tabWidthField= new IntegerFieldEditor(PreferenceConstants.P_TAB_WIDTH, "&Tab width:", getFieldEditorParent());
+        tabWidthField.setValidRange(1, 16);
+        tabWidthField.setTextLimit(2);
+        tabWidthField.setEmptyStringAllowed(false);
         addField(tabWidthField);
 
-	final FontFieldEditor fontField= new FontFieldEditor(PreferenceConstants.P_SOURCE_FONT,
-		"Source font:", getFieldEditorParent());
-	addField(fontField);
+        final FontFieldEditor fontField= new FontFieldEditor(PreferenceConstants.P_SOURCE_FONT, "Source font:", getFieldEditorParent());
+        addField(fontField);
 
-	// Don't need a preference store listener here; the UniversalEditor already listens
-	// to the preference store, and takes the necessary actions. Moreover, some preference
-	// changes require some resource management that only it can do properly (e.g. it must
-	// only dispose of the old Font *after* telling the ITextViewer to use the new one).
+        // Don't need a preference store listener here; the UniversalEditor already listens
+        // to the preference store, and takes the necessary actions. Moreover, some preference
+        // changes require some resource management that only it can do properly (e.g. it must
+        // only dispose of the old Font *after* telling the ITextViewer to use the new one).
     }
 
-    public void init(IWorkbench workbench) {}
+    public void init(IWorkbench workbench) {
+    }
 }

f903f20e859ae80bc4f43f8e4f931ec1beaf0007
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-05-09 19:59:25 +0000

    Removed IMarker from API methods, since it seems that the markers get created/destroyed as a side-effect of creating the breakpoints themselves (at least as far as the JDT and Java-hosted languages are concerned).

--- src/org/eclipse/imp/services/IToggleBreakpointsHandler.java
+++ src/org/eclipse/imp/services/IToggleBreakpointsHandler.java
@@ -1,14 +1,13 @@
 package org.eclipse.imp.services;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
 import org.eclipse.imp.language.ILanguageService;
 
 public interface IToggleBreakpointsHandler extends ILanguageService {
-	void clearLineBreakpoint(IFile file, int lineNumber, IMarker marker);
-	void setLineBreakpoint(IFile file, int lineNumber, IMarker marker);
-	void disableLineBreakpoint(IFile file, int lineNumber, IMarker marker);
-	void enableLineBreakpoint(IFile file, int lineNumber, IMarker marker);
+	void clearLineBreakpoint(IFile file, int lineNumber);
+	void setLineBreakpoint(IFile file, int lineNumber);
+	void disableLineBreakpoint(IFile file, int lineNumber);
+	void enableLineBreakpoint(IFile file, int lineNumber);
 	// TODO
 	//void toggleEntryBreakpoint(IFile file, int lineNumber);
 }

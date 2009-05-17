952ee5355c92a5b1bd7c7dd0f7e9c43e35a7d5be
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-05-17 16:11:30 +0000

    New class to house editor action ID constants.

---
+++ src/org/eclipse/imp/editor/IEditorActionDefinitionIds.java
@@ -0,0 +1,17 @@
+package org.eclipse.imp.editor;
+
+public interface IEditorActionDefinitionIds {
+    /**
+     * Action definition id of the collapse members action
+     * (value: <code>"org.eclipse.imp.editor.folding.collapseMembers"</code>).
+     * @since 0.1
+     */
+    public static final String FOLDING_COLLAPSE_MEMBERS= "org.eclipse.imp.editor.folding.collapseMembers"; //$NON-NLS-1$
+
+    /**
+     * Action definition id of the collapse comments action
+     * (value: <code>"org.eclipse.imp.editor.folding.collapseComments"</code>).
+     * @since 0.1
+     */
+    public static final String FOLDING_COLLAPSE_COMMENTS= "org.eclipse.imp.editor.folding.collapseComments"; //$NON-NLS-1$
+}

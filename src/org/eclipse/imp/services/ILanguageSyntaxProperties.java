b73bd58b689ac7cf8b3bfd500c26460e37c9a969
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-06-26 22:05:07 +0000

    Added JavaDoc comments to methods that didn't have them.

--- src/org/eclipse/imp/services/ILanguageSyntaxProperties.java
+++ src/org/eclipse/imp/services/ILanguageSyntaxProperties.java
@@ -7,13 +7,17 @@
 *
 * Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
 *******************************************************************************/
 
 package org.eclipse.imp.services;
 
 import org.eclipse.imp.language.ILanguageService;
 
+/**
+ * Describes certain syntactic properties of the language used in support of certain editor features, such
+ * as toggling comments, fence matching, and so on.
+ * @author rfuhrer@watson.ibm.com
+ */
 public interface ILanguageSyntaxProperties extends ILanguageService {
     /**
      * if null, the language has no single-line comment syntax
@@ -21,10 +25,20 @@ public interface ILanguageSyntaxProperties extends ILanguageService {
      */
     public String getSingleLineCommentPrefix();
 
+    /**
+     * @return the character sequence that starts a block comment, if the language has syntax for that, else null
+     */
     public String getBlockCommentStart();
 
+    /**
+     * @return the character sequence for a line in the middle of a block comment, if the language has syntax
+     * for that, else null
+     */
     public String getBlockCommentContinuation();
 
+    /**
+     * @return the character sequence that ends a block comment, if the language has syntax for that, else null
+     */
     public String getBlockCommentEnd();
 
     /**
@@ -44,7 +58,15 @@ public interface ILanguageSyntaxProperties extends ILanguageService {
      */
     public String[][] getFences();
 
+    /**
+     * @return the set of characters that are legal in an identifier in this language
+     */
     public String getIdentifierConstituentChars();
 
+    /**
+     * @param ident an identifier
+     * @return the ordered set of character offsets to the individual components (e.g. words) of an identifier.
+     * May be null.
+     */
     public int[] getIdentifierComponents(String ident);
 }

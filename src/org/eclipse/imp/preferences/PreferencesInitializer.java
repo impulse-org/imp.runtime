863912d01ba375f32d714642b17ee9d54734ac73
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-04-02 18:22:59 +0000

    Cosmetic changes.

--- src/org/eclipse/imp/preferences/PreferencesInitializer.java
+++ src/org/eclipse/imp/preferences/PreferencesInitializer.java
@@ -7,24 +7,21 @@
 *
 * Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
 *******************************************************************************/
 
 package org.eclipse.imp.preferences;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 
-public abstract class PreferencesInitializer extends AbstractPreferenceInitializer
-{
+public abstract class PreferencesInitializer extends AbstractPreferenceInitializer {
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
 	 */
 	public abstract void initializeDefaultPreferences();
 
-	
 	/**
-	 * For the given preferences level, clear the valeus of preferences that
+	 * For the given preferences level, clear the values of preferences that
 	 * are initialized by this initializer.
 	 * 
 	 * @param level	The name of the preferences level for which preference

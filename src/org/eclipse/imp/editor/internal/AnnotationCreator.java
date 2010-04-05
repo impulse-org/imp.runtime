d76394d50b7bbdfd54e400fbd6379246457f0569
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-04-05 23:28:11 +0000

    Removed unsightly block of bogus commented-out code.

--- src/org/eclipse/imp/editor/internal/AnnotationCreator.java
+++ src/org/eclipse/imp/editor/internal/AnnotationCreator.java
@@ -7,7 +7,6 @@
 *
 * Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
 *******************************************************************************/
 
 package org.eclipse.imp.editor.internal;
@@ -41,18 +40,6 @@ public class AnnotationCreator implements IMessageHandler {
             int startCol, int endCol,
             int startLine, int endLine) {
         
-//    public void handleMessage(int errorCode, int [] msgLocation, int[] errorLocation, String filename, String [] errorInfo) {
-//        int offset = msgLocation[IMessageHandler.OFFSET_INDEX],
-//            length = msgLocation[IMessageHandler.LENGTH_INDEX];
-//        String message = "";
-//        if (errorCode != ParseErrorCodes.DELETION_CODE &&
-//            errorCode != ParseErrorCodes.MISPLACED_CODE)
-//        {        
-//            for (int i = 0; i < errorInfo.length; i++)
-//                message += errorInfo[i] + " ";
-//        }
-//        message += ParseErrorCodes.errorMsgText[errorCode];
-
         IAnnotationModel model= fEditor.getDocumentProvider().getAnnotationModel(fEditor.getEditorInput());
         Annotation annotation= new Annotation(fAnnotationType, false, message);
         

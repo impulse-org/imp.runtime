9672d1190dd8cfb6f868f687f547d9e0a44f537b
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-04-05 15:11:40 +0000

    Added some missing JavaDoc/

--- src/org/eclipse/imp/services/IContentProposer.java
+++ src/org/eclipse/imp/services/IContentProposer.java
@@ -7,7 +7,6 @@
 *
 * Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
 *******************************************************************************/
 
 package org.eclipse.imp.services;
@@ -17,22 +16,29 @@ import org.eclipse.imp.parser.IParseController;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 
-/*
- * Licensed Materials - Property of IBM,
- * (c) Copyright IBM Corp. 1998, 2004  All Rights Reserved
- */
-
-
 /**
  * This interface is to be implemented by contributors to the org.eclipse.imp.runtime.contentProposer extension point.
  * The Universal IDE Editor will locate a suitable parser for the language being edited.
- * The result of the parser, an Ast describing the syntactical elements in the input, is cached
+ * The result of the parser, an AST describing the syntactical elements in the input, is cached
  * and used when the mouse is hovered over elements in the editor.
  * 
  * @author Claffra
  */
 public interface IContentProposer extends ILanguageService {
-
+    /**
+     * Returns an array of content proposals applicable relative to the AST of the given
+     * parse controller at the given position.
+     * 
+     * The ITextViewer is provided for purposes such as accessing the IDocument with respect
+     * to which content proposals are sought.
+     * 
+     * @param controller    A parse controller from which the AST of the document being edited
+     *                      can be obtained
+     * @param int           The offset for which content proposals are sought
+     * @param viewer        The viewer in which the document represented by the AST in the given
+     *                      parse controller is being displayed (may be null for some implementations)
+     * @return              An array of completion proposals applicable relative to the AST of the given
+     *                      parse controller at the given position
+     */
     public ICompletionProposal[] getContentProposals(IParseController controller, int offset, ITextViewer viewer);
-
 }

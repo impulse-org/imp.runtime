9f8dc06bf2586578e27c9274c287ef2c4ff8174f
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-04-16 01:46:18 +0000

    Fixed race condition that caused updates to be dropped when the parser is very slow compared to the presentation repairer, the latter of which consumes the damage region in an update before parsing is complete. Rearranged the diagnostic code to analyze the source of a malformed set of StyleRanges, and tries somewhat harder to fix it.

--- src/org/eclipse/imp/editor/internal/PresentationController.java
+++ src/org/eclipse/imp/editor/internal/PresentationController.java
@@ -13,6 +13,8 @@ package org.eclipse.imp.editor.internal;
 
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
@@ -109,22 +111,57 @@ public class PresentationController implements IModelListener {
         IRegion bigRegion= fColorer.calculateDamageExtent(region, fParseCtlr);
 
         if (bigRegion != null) {
-            fWorkItems.push(bigRegion);
+            synchronized (fWorkItems) {
+                boolean redundant= false;
+                for(IRegion dr : fWorkItems) {
+                    if (contains(bigRegion, dr)) {
+                        redundant= true;
+                    }
+                }
+                if (!redundant) {
+//                  System.out.println("damage(): got irredundant damage region: " + bigRegion.getOffset() + ":" + bigRegion.getLength());
+                    fWorkItems.push(bigRegion);
+                }
+            }
         }
     }
 
+    private boolean contains(IRegion r1, IRegion r2) {
+        return r2.getOffset() <= r1.getOffset() && r2.getOffset() + r2.getLength() >= r1.getOffset() + r1.getLength();
+    }
+
     public void update(IParseController controller, IProgressMonitor monitor) {
+//        try {
+//            throw new Exception();
+//        } catch (Exception e) {
+//            System.out.println("Entered PresentationController.update()");
+//            e.printStackTrace(System.out);
+//        }
         if (!monitor.isCanceled()) {
-//            if (fWorkItems.size() == 0) {
-//                ConsoleUtil.findConsoleStream(PresentationController.CONSOLE_NAME).println("PresentationController.update() called, but no damage in the work queue?");
-//            }
+//          if (fWorkItems.size() == 0) {
+//              ConsoleUtil.findConsoleStream(PresentationController.CONSOLE_NAME).println("PresentationController.update() called, but no damage in the work queue?");
+//          }
             synchronized (fWorkItems) {
+                if (fWorkItems.size() == 0) {
+                    // TODO Shouldn't need to re-color the entire source file here.
+                    // This is intended to handle the case that the parser finishes *after*
+                    // the PresentationRepairer asks for an update().
+                    // We could do a more focused update, if we knew what part of the file had changed.
+//                  System.out.println("PresentationController.update() called, but no work items; reprocessing entire document");
+                    fWorkItems.add(new Region(0, fSourceViewer.getDocument().getLength()));
+                }
+                // TODO Optimization: when there are multiple work items, control redrawing explicitly.
+                // See JavaDoc regarding ITextViewer.changeTextPresentation()'s 2nd argument.
+                // Probably not very common (only refactoring or search/replace?), but perhaps worthwhile.
                 for(int n= fWorkItems.size() - 1; !monitor.isCanceled() && n >= 0; n--) {
                     Region damage= (Region) fWorkItems.get(n);
+//                  System.out.println(">>> Processing damage region: " + damage.getOffset() + ":" + damage.getLength());
                     changeTextPresentationForRegion(controller, monitor, damage);
                 }
-                if (!monitor.isCanceled())
+                // TODO Remove the work items we actually processed, whether the monitor was canceled or not
+                if (!monitor.isCanceled()) {
                     fWorkItems.removeAllElements();
+                }
             }
         }
     }
@@ -143,7 +180,11 @@ public class PresentationController implements IModelListener {
         ISourcePositionLocator locator= parseController.getSourcePositionLocator();
 
         aggregateTextPresentation(parseController, monitor, damage, presentation, locator);
-        if (!monitor.isCanceled() && !presentation.isEmpty()) {
+        if (monitor.isCanceled()) {
+            System.err.println("Ignored cancelled presentation update");
+        } else if (presentation.isEmpty()) {
+            System.err.println("Ignored empty presentation update");
+        } else {
             submitTextPresentation(presentation);
         }
     }
@@ -172,41 +213,39 @@ public class PresentationController implements IModelListener {
 
     private void changeTokenPresentation(IParseController controller, TextPresentation presentation, Object token, ISourcePositionLocator locator) {
         TextAttribute attribute= fColorer.getColoring(controller, token);
-        
+
         StyleRange styleRange= new StyleRange(locator.getStartOffset(token), locator.getEndOffset(token) - locator.getStartOffset(token) + 1,
                 attribute == null ? null : attribute.getForeground(),
                 attribute == null ? null : attribute.getBackground(),
                 attribute == null ? SWT.NORMAL : attribute.getStyle());
-        
-       // SMS 21 Jun 2007:  negative (possibly 0) length style ranges seem to cause problems;
-        // but if you have one it should lead to an IllegalArgumentException in changeTextPresentation(..)
-        if (styleRange.length <= 0 || styleRange.start + styleRange.length > this.fSourceViewer.getDocument().getLength()) {
+
+        // Negative (possibly 0) length style ranges will cause an 
+        // IllegalArgumentException in changeTextPresentation(..)
+        if (styleRange.length <= 0 || styleRange.start + styleRange.length > fSourceViewer.getDocument().getLength()) {
+//          System.err.println("Omitting token '" + token + "' w/ empty style range: " + styleRange.start + ":" + styleRange.length);
         } else {
             presentation.addStyleRange(styleRange);
         }
     }
 
     private void submitTextPresentation(final TextPresentation presentation) {
+        if (fSourceViewer == null) {
+            return;
+        }
+
+        final int docLength= (fSourceViewer.getDocument() != null) ? fSourceViewer.getDocument().getLength() : 0;
+        final TextPresentation newPresentation= fixPresentation(presentation, docLength, false /*sort?*/);
+
         Display.getDefault().asyncExec(new Runnable() {
             public void run() {
-            	int charCount;
-
-            	if (fSourceViewer != null && fSourceViewer.getDocument() != null) {
-            		charCount = fSourceViewer.getDocument().getLength();
-            	} else {
-            		charCount = 0;
-            	}
-
-            	TextPresentation newPresentation= trimPresentationToTextExtent(presentation, charCount);
-
-        	    // SMS 21 Jun 2007 added try-catch block
-        	    // Note:  It doesn't work to just eat the exception here; if there is a problematic token
+        	    // Note: It doesn't work to just eat the exception here; if there is a problematic token
         	    // then an exception is likely to arise downstream in the computation anyway
         	    try {
-        	    	if (fSourceViewer != null)
+        	    	if (fSourceViewer != null) {
         	    		fSourceViewer.changeTextPresentation(newPresentation, true);
+        	    	}
         	    } catch (IllegalArgumentException e) {
-        	        logStyleRangeError(presentation, charCount, e);
+        	        diagnoseStyleRangeError(presentation, docLength, e);
         	        throw e;
         	    }
             }
@@ -214,26 +253,94 @@ public class PresentationController implements IModelListener {
     }
 
     /**
-     * Trims the given presentation as necessary to one whose final range doesn't
-     * extend beyond the last character in the source text.
+     * Adjusts the StyleRanges in the given presentation as necessary to ones that
+     * should be acceptable to ITextViewer.changeTextPresentation().
+     * In particular, no range will extend beyond the end of the source text,
+     * and their lengths will all be positive.
+     * Optionally, will also sort the ranges, and ensure that they don't overlap. 
      */
-    private TextPresentation trimPresentationToTextExtent(final TextPresentation presentation, int charCount) {
+    private TextPresentation fixPresentation(final TextPresentation presentation, int docLen, boolean sort) {
+        if (checkPresentation(presentation, docLen)) {
+            return presentation;
+        }
         int lastStart = presentation.getLastStyleRange().start;
         int lastLength = presentation.getLastStyleRange().length;
         int end = lastStart + lastLength;
-        TextPresentation newPresentation = null;
-        if (end >= charCount) {
-            newPresentation = new TextPresentation();
-            Iterator presIt = presentation.getAllStyleRangeIterator();
-            while (presIt.hasNext()) {
-                StyleRange nextRange = (StyleRange) presIt.next();
-                if (nextRange.start + nextRange.length < charCount)
-                    newPresentation.addStyleRange(nextRange);
+
+        List<StyleRange> newRanges= new ArrayList<StyleRange>(presentation.getDenumerableRanges());
+
+        // Phase 1: Collect all ranges in a sortable data structure and trim each one
+        // to ensure it lies within the document bounds.
+        Iterator presIt = presentation.getAllStyleRangeIterator();
+        while (presIt.hasNext()) {
+            StyleRange nextRange = (StyleRange) presIt.next();
+            if (nextRange.start < docLen) {
+                if (nextRange.start + nextRange.length > docLen) {
+                    nextRange.length= docLen - nextRange.start;
+                }
+                newRanges.add(nextRange);
+            } else {
+                // discard range that lies completely outside the document
             }
-        } else {
-            newPresentation = presentation;
+        }
+
+        // Phase 2: sort the ranges by their start offset
+        Collections.sort(newRanges, new Comparator<StyleRange>() {
+            public int compare(StyleRange o1, StyleRange o2) {
+                return o1.start - o2.start;
+            }
+        });
+
+        // Phase 3: check for overlap of adjacent ranges and trim as needed
+        StyleRange prevRange= newRanges.get(0);
+        for(int i=1; i < newRanges.size(); i++) {
+            StyleRange currRange= newRanges.get(i);
+            if (currRange.start < prevRange.start + prevRange.length) {
+                prevRange.length= currRange.start - prevRange.start;
+            }
+            prevRange= currRange;
+        }
+
+        // Phase 4: remove any ranges that are now empty (as a result of trimming in Phase 3)
+        for(Iterator<StyleRange> ri= newRanges.iterator(); ri.hasNext(); ) {
+            StyleRange r= ri.next();
+            if (r.length <= 0) {
+                ri.remove();
+            }
+        }
+
+        // Final phase: construct new TextPresentation
+        TextPresentation newPresentation = new TextPresentation();
+        for(StyleRange r: newRanges) {
+            newPresentation.addStyleRange(r);
         }
         return newPresentation;
+    }
+
+    /**
+     * A fail-fast checker that returns false to indicate whether any problems
+     * necessitate a fixing pass over the StyleRanges.
+     */
+    private boolean checkPresentation(TextPresentation presentation, int docLen) {
+        Iterator<StyleRange> presIt = presentation.getAllStyleRangeIterator();
+        int end= -1;
+
+        while (presIt.hasNext()) {
+            StyleRange r= presIt.next();
+            int rangeStart= r.start;
+            int rangeLen= r.length;
+            if (rangeStart < end) {
+                return false;
+            }
+            if (rangeLen < 1) {
+                return false;
+            }
+            if (rangeStart + rangeLen > docLen) {
+                return false;
+            }
+            end= Math.max(end, rangeStart+rangeLen);
+        }
+        return true; // ok
     }
 
     /**
@@ -243,51 +350,55 @@ public class PresentationController implements IModelListener {
      * Try to determine the real cause of the problem, and add an appropriate message
      * for the exception in the plugin log.
      */
-    private void logStyleRangeError(final TextPresentation presentation, int charCount, IllegalArgumentException e) {
-        // Possible causes (not necessarily exclusive:
+    private void diagnoseStyleRangeError(final TextPresentation presentation, int charCount, IllegalArgumentException e) {
+        // Possible causes (not necessarily complete):
         // - negative length in a style range
         // - overlapping ranges
         // - range extends beyond last character in file
         Iterator<StyleRange> ranges = presentation.getAllStyleRangeIterator();
         List<StyleRange> rangesList = new ArrayList<StyleRange>();
-        while(ranges.hasNext())
-        	rangesList.add((StyleRange)ranges.next());
-        String explanation = null;
+        while (ranges.hasNext()) {
+        	rangesList.add((StyleRange) ranges.next());
+        }
+        StringBuilder explanation = new StringBuilder();
         if (rangesList.size() > 0) {
         	StyleRange firstRange = rangesList.get(0);
-        	if (firstRange.length < 0)
-        		explanation = "Style range with start = " + firstRange.start + " has negative length = " + firstRange.length;
-        	if (explanation == null) {
-            	for (int i = 1; i < rangesList.size(); i++) {
-            		int currStart = rangesList.get(i).start;
-            		int currLength = rangesList.get(i).length;
-            		if (currLength <0) {
-            			explanation = "Style range with start = " + currStart + " has negative length = " + currLength;
-            			break;
-            		}
-            		
-            		int prevStart = rangesList.get(i-1).start;
-            		int prevLength = rangesList.get(i-1).length;
-            		if (prevStart + prevLength - 1 >= currStart) {
-            			explanation = "Style range with start = " + prevStart + " and length = " + prevLength + 
-            				"overlaps style range with start = " + currStart;
-            			break;
-            		}
-            	}
+
+        	if (firstRange.length < 0) {
+        		explanation.append("Style range with start = " + firstRange.start + " has negative length = " + firstRange.length);
         	}
-        	if (explanation == null) {
-            	int finalStart = presentation.getLastStyleRange().start;
-            	int finalLength = presentation.getLastStyleRange().length;
-            	int finalEnd = finalStart + finalLength;
-            	if (finalEnd >= charCount) {
-        			explanation = "Final style range with start = " + finalStart + " and length = " + finalLength + 
-        				"extends beyond last character (character count = " + charCount + ")";
+        	StyleRange prevRange= firstRange;
+        	for (int i = 1; i < rangesList.size(); i++) {
+        	    StyleRange currRange= rangesList.get(i);
+                int currStart = currRange.start;
+        	    int currLength = currRange.length;
+        	    if (currLength < 0) {
+        	        explanation.append("Style range with start = " + currStart + " has negative length = " + currLength);
+        	        break;
+        	    }
+
+        	    int prevStart = prevRange.start;
+        	    int prevLength = prevRange.length;
+        	    if (prevStart + prevLength - 1 >= currStart) {
+        	        explanation.append("Style range with start = " + prevStart + " and length = " + prevLength +
+        	                "overlaps style range with start = " + currStart);
+        	        break;
             	}
+        	    prevRange= currRange;
         	}
-        	if (explanation == null)
-        		explanation = "Cause not identified";        	    		
+
+        	int finalStart = presentation.getLastStyleRange().start;
+        	int finalLength = presentation.getLastStyleRange().length;
+        	int finalEnd = finalStart + finalLength;
+        	if (finalEnd >= charCount) {
+        	    explanation.append("Final style range with start = " + finalStart + " and length = " + finalLength + 
+        	            "extends beyond last character (character count = " + charCount + ")");
+        	}
+        	if (explanation.length() == 0) {
+        		explanation.append("Cause not identified");
+        	}
         }
- 
-        ErrorHandler.logError("PresentationController.submitTextPresentation(): IllegalArgumentException: " + explanation, e);
+
+        ErrorHandler.logError("Malformed TextPresentation:" + explanation, e);
     }
 }

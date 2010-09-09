f858d6a6c58294c5ee457aaf45f287d871c25fdd
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-09-09 17:21:23 +0000

    Patch from bug #322427: merged in AnnotationHoverBase, which consisted almost entirely of static utility methods, with some duplication of code here.

--- src/org/eclipse/imp/utils/AnnotationUtils.java
+++ src/org/eclipse/imp/utils/AnnotationUtils.java
@@ -7,15 +7,19 @@
  *
  * Contributors:
  *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
  *******************************************************************************/
 
 package org.eclipse.imp.utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.eclipse.imp.editor.MarkOccurrencesAction;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.Position;
@@ -23,11 +27,29 @@ import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.projection.AnnotationBag;
+import org.eclipse.jface.text.source.projection.ProjectionAnnotation;
 
 public class AnnotationUtils {
-    private AnnotationUtils() {
+    // Following is just used for debugging to discover new annotation types to filter out
+    //  private static Set<String> fAnnotationTypes= new HashSet<String>();
+
+    private static Set<String> sAnnotationTypesToFilter= new HashSet<String>();
+
+    static {
+        sAnnotationTypesToFilter.add("org.eclipse.ui.workbench.texteditor.quickdiffUnchanged");
+        sAnnotationTypesToFilter.add("org.eclipse.ui.workbench.texteditor.quickdiffChange");
+        sAnnotationTypesToFilter.add("org.eclipse.ui.workbench.texteditor.quickdiffAddition");
+        sAnnotationTypesToFilter.add("org.eclipse.ui.workbench.texteditor.quickdiffDeletion");
+        sAnnotationTypesToFilter.add("org.eclipse.debug.core.breakpoint");
+        sAnnotationTypesToFilter.add(MarkOccurrencesAction.OCCURRENCE_ANNOTATION);
+        sAnnotationTypesToFilter.add(ProjectionAnnotation.TYPE);
     }
 
+    private AnnotationUtils() { }
+
+    /**
+     * @return a nicely-formatted plain text string for the given set of annotations
+     */
     public static String formatAnnotationList(List<Annotation> annotations) {
         if (annotations != null) {
             if (annotations.size() == 1) {
@@ -52,61 +74,30 @@ public class AnnotationUtils {
         return null;
     }
 
-    public static IAnnotationModel getAnnotationModel(ISourceViewer viewer) {
-        // if (viewer instanceof ISourceViewerExtension2) {
-        // ISourceViewerExtension2 extension= (ISourceViewerExtension2) viewer;
-        //
-        // return extension.getVisualAnnotationModel();
-        // }
-        return viewer.getAnnotationModel();
-    }
-
     /**
-     * Check preferences, etc., to determine whether this annotation is actually showing. (Don't want to show a hover
-     * for a non-visible annotation.)
-     * 
-     * @param annotation
-     * @param position
-     * @param messagesAtPosition
-     * @return
+     * @return true, if the given Annotation and Position are redundant, given the annotation
+     * information in the given Map
      */
-    public static boolean includeAnnotation(Annotation annotation, Position position) {
-        return true;
-    }
+	public static boolean isDuplicateAnnotation(Map<Integer, List<String>> map, Annotation annotation, Position position) {
+		if (!map.containsKey(position.offset)) {
+			List<String> list = new ArrayList<String>();
+			list.add(annotation.getText());
+			map.put(position.offset, list);
+			return false;
+		}
 
-    public static List<Annotation> getAnnotations(ISourceViewer viewer, IPositionPredicate posPred) {
-        IAnnotationModel model= getAnnotationModel(viewer);
-        if (model == null)
-            return null;
-        List<Annotation> annotations= new ArrayList<Annotation>();
-        Iterator<Annotation> iterator= model.getAnnotationIterator();
-        while (iterator.hasNext()) {
-            Annotation annotation= (Annotation) iterator.next();
-            Position position= model.getPosition(annotation);
-            // System.out.println("Checking annotation @ " + position.offset + ":" + position.length + " => " +
-            // annotation.getText());
-            if (annotation.getType().equals("org.eclipse.ui.workbench.texteditor.quickdiffUnchanged"))
-                continue;
-            if (position == null)
-                continue;
-            if (!posPred.matchPosition(position))
-                continue;
-            if (annotation instanceof AnnotationBag) {
-                AnnotationBag bag= (AnnotationBag) annotation;
-                for(Iterator<Annotation> e= bag.iterator(); e.hasNext(); ) {
-                    Annotation bagAnnotation= (Annotation) e.next();
-                    position= model.getPosition(bagAnnotation);
-                    if (position != null && includeAnnotation(bagAnnotation, position))
-                        annotations.add(bagAnnotation);
-                }
-            } else {
-                if (includeAnnotation(annotation, position))
-                    annotations.add(annotation);
-            }
-        }
-        return annotations;
-    }
+		List<String> list = map.get(position.offset);
+		if (!list.contains(annotation.getText())) {
+			list.add(annotation.getText());
+			return false;
+		}
 
+		return true;
+	}
+
+	/**
+	 * @return the list of Annotations that reside at the given line for the given ISourceViewer
+	 */
     public static List<Annotation> getAnnotationsForLine(ISourceViewer viewer, final int line) {
         final IDocument document= viewer.getDocument();
         IPositionPredicate posPred= new IPositionPredicate() {
@@ -117,6 +108,9 @@ public class AnnotationUtils {
         return getAnnotations(viewer, posPred);
     }
 
+    /**
+     * @return the list of Annotations that reside at the given offset for the given ISourceViewer
+     */
     public static List<Annotation> getAnnotationsForOffset(ISourceViewer viewer, final int offset) {
         IPositionPredicate posPred= new IPositionPredicate() {
             public boolean matchPosition(Position p) {
@@ -126,15 +120,99 @@ public class AnnotationUtils {
         return getAnnotations(viewer, posPred);
     }
 
+    /**
+     * @return true, if the given Position resides at the given line of the given IDocument
+     */
     public static boolean offsetIsAtLine(Position position, IDocument document, int line) {
         if (position.getOffset() > -1 && position.getLength() > -1) {
             try {
+            	// RMF 11/10/2006 - This used to add 1 to the line computed by the document,
+                // which appears to be bogus. First, it didn't work right (annotation hovers
+                // never appeared); second, the line passed in comes from the Eclipse
+                // framework, so it should be consistent (wrt the index base) with what the
+                // IDocument API provides.
                 int posLine= document.getLineOfOffset(position.getOffset());
-                // System.out.println(" at line " + posLine);
                 return line == posLine;
             } catch (BadLocationException x) {
             }
         }
         return false;
     }
+
+    /**
+     * @return the IAnnotationModel for the given ISourceViewer, if any
+     */
+    // TODO get rid of this one-line wrapper
+    public static IAnnotationModel getAnnotationModel(ISourceViewer viewer) {
+        // if (viewer instanceof ISourceViewerExtension2) {
+        // ISourceViewerExtension2 extension= (ISourceViewerExtension2) viewer;
+        //
+        // return extension.getVisualAnnotationModel();
+        // }
+        return viewer.getAnnotationModel();
+    }
+
+    /**
+     * @return the list of Annotations on the given ISourceViewer that satisfy the given
+     * IPositionPredicate and that are worth showing to the user as text (e.g., ignoring
+     * debugger breakpoint annotations and source folding annotations)
+     */
+    public static List<Annotation> getAnnotations(ISourceViewer viewer, IPositionPredicate posPred) {
+		IAnnotationModel model = getAnnotationModel(viewer);
+		if (model == null)
+			return null;
+		List<Annotation> annotations = new ArrayList<Annotation>();
+		Iterator<?> iterator = model.getAnnotationIterator();
+
+		Map<Integer, List<String>> map = new HashMap<Integer, List<String>>();
+
+		while (iterator.hasNext()) {
+			Annotation annotation = (Annotation) iterator.next();
+			Position position = model.getPosition(annotation);
+			
+			if (position == null)
+				continue;
+			if (!posPred.matchPosition(position))
+				continue;
+			if (annotation instanceof AnnotationBag) {
+				AnnotationBag bag = (AnnotationBag) annotation;
+				for (Iterator<?> e = bag.iterator(); e.hasNext(); ) {
+					Annotation bagAnnotation = (Annotation) e.next();
+					position = model.getPosition(bagAnnotation);
+					if (position != null
+							&& includeAnnotation(bagAnnotation, position)
+							&& !isDuplicateAnnotation(map, bagAnnotation, position))
+						annotations.add(bagAnnotation);
+
+				}
+			} else {
+				if (includeAnnotation(annotation, position)
+						&& !isDuplicateAnnotation(map, annotation, position)) {
+					annotations.add(annotation);
+				}
+			}
+		}
+		return annotations;
+	}
+
+    /**
+     * Check preferences, etc., to determine whether this annotation is actually showing.
+     * (Don't want to show a hover for a non-visible annotation.)
+     * @param annotation
+     * @param position
+     * @return
+     */
+    private static boolean includeAnnotation(Annotation annotation, Position position) {
+        return !sAnnotationTypesToFilter.contains(annotation.getType());
+    }
+
+    /**
+     * @see IVerticalRulerHover#getHoverInfo(ISourceViewer, int)
+     */
+    public String getHoverInfo(ISourceViewer sourceViewer, int lineNumber) {
+        List<Annotation> annotations = getAnnotationsForLine(sourceViewer, lineNumber);
+
+        return formatAnnotationList(annotations);
+    }
 }
+

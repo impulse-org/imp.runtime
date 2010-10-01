bf654d4e8e6740d353030535e528a7567dc7d900
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-10-01 19:21:43 +0000

    Added missing copyright header.

--- src/org/eclipse/imp/editor/quickfix/IAnnotation.java
+++ src/org/eclipse/imp/editor/quickfix/IAnnotation.java
@@ -1,12 +1,20 @@
+/*******************************************************************************
+* Copyright (c) 2010 IBM Corporation.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+*******************************************************************************/
+
 package org.eclipse.imp.editor.quickfix;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.imp.editor.UniversalEditor;
 
-
-
 public interface IAnnotation {
-
 	static final int WARNING = IStatus.WARNING;
 	static final int ERROR = IStatus.ERROR;
 	static final int INFO = IStatus.INFO;

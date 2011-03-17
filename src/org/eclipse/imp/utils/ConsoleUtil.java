2b9b07dcf6be3c3e2f975d3c4e1161a7979c83ec
Robert M. Fuhrer <rmfuhrer@optonline.net>
2011-03-17 20:17:38 +0000

    Added a bit of new convenience API.

--- src/org/eclipse/imp/utils/ConsoleUtil.java
+++ src/org/eclipse/imp/utils/ConsoleUtil.java
@@ -1,15 +1,13 @@
 /*******************************************************************************
-* Copyright (c) 2008 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
-*******************************************************************************/
-
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.imp.utils;
 
@@ -23,27 +21,44 @@ import org.eclipse.ui.console.MessageConsoleStream;
 
 public class ConsoleUtil {
     public static MessageConsole findConsole(String name) {
-        MessageConsole myConsole= null;
         final IConsoleManager consoleManager= ConsolePlugin.getDefault().getConsoleManager();
         IConsole[] consoles= consoleManager.getConsoles();
+
         for(int i= 0; i < consoles.length; i++) {
             IConsole console= consoles[i];
             if (console.getName().equals(name))
-                myConsole= (MessageConsole) console;
+                return (MessageConsole) console;
         }
-        if (myConsole == null) {
-            myConsole= new MessageConsole(name, null);
-            consoleManager.addConsoles(new IConsole[] { myConsole });
-        }
-        consoleManager.showConsoleView(myConsole);
+        MessageConsole myConsole= new MessageConsole(name, null);
+
+        consoleManager.addConsoles(new IConsole[] { myConsole });
+
         return myConsole;
+    }
+
+    public static MessageConsole findAndShowConsole(String name) {
+        MessageConsole console= findConsole(name);
+
+        showConsole(console);
+        return console;
+    }
+
+    public static PrintStream getPrintStreamFor(MessageConsole console) {
+        MessageConsoleStream consStream= console.newMessageStream();
+        PrintStream ps= new PrintStream(consStream);
+
+        return ps;
     }
 
     public static PrintStream findConsoleStream(String name) {
         MessageConsole myConsole= findConsole(name);
-        MessageConsoleStream consStream= myConsole.newMessageStream();
-        PrintStream ps= new PrintStream(consStream);
 
-        return ps;
+        return getPrintStreamFor(myConsole);
+    }
+
+    public static void showConsole(MessageConsole console) {
+        final IConsoleManager consoleManager = ConsolePlugin.getDefault().getConsoleManager();
+
+        consoleManager.showConsoleView(console);
     }
 }
